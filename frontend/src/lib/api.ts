bfb6caf8bed8410714ffc2aba8bde30308d3c880
Vijay Ambade <ambadevijay999@gmail.com>
2026-05-29 23:41:15 +0530

    frontend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+// const BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const BASE = process.env.NEXT_PUBLIC_API_URL;
 
 export const api = axios.create({ baseURL: BASE });
 
