3b27aa7cc9bfcbad650e790f1bcf01517274e2a8
Vijay Ambade <ambadevijay999@gmail.com>
2026-05-30 12:04:20 +0530

    fixed api url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 // const BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
-const BASE = process.env.NEXT_PUBLIC_API_URL;
+const BASE  = process.env.NEXT_PUBLIC_API_URL;
+
+export async function apicall(path: string,options={}) {
+  const res = await fetch(`${BASE}${path}`,options);
+  if (!res.ok) {
+    throw new Error(`API request failed: ${res.status} ${res.statusText}`);
+  }
+  return res.json();
+}
 
 export const api = axios.create({ baseURL: BASE });
 
